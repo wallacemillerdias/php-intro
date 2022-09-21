2430d7ab1b107c632308901f96db60c94de5ce40
wallacemillerdias <wallacemillerdias@gmail.com>
2022-09-21 20:47:41 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Formulário de inscrição</title>
+</head>
+<body>
+    <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+    <form action="script.php" method="POST">
+    <p>Seu nome: <input type="text" name="nome" /></p>
+    <p>Sua idade: <input type="number" name="idade" /></p>
+    <p><input type="submit" value="" /></p>
+
+
+    </form>
+</body>
+</html>
